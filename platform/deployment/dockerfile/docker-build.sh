aae2ff556381b2c0be424c00ee3a20ec4bf0be32
zhaoyang <zygideon@qq.com>
2025-09-30 10:39:54 +0800

    installlllll

--- platform/deployment/dockerfile/docker-build.sh
+++ platform/deployment/dockerfile/docker-build.sh
@@ -12,8 +12,12 @@ docker build --platform linux/amd64 -f Dockerfile.workflow -t harbor.kunxiangtec
 #构建gateway
 docker build --platform linux/amd64 -f Dockerfile.gateway -t harbor.kunxiangtech.com:8443/kb-platform/gateway:${VERSION} ../../
 
+#构建frontend
+docker build --platform linux/amd64 -f Dockerfile.frontend -t harbor.kunxiangtech.com:8443/kb-platform/frontend:${VERSION} ../../../
+
 #推送
 docker push harbor.kunxiangtech.com:8443/kb-platform/kb-service:${VERSION}
 docker push harbor.kunxiangtech.com:8443/kb-platform/iam:${VERSION}
 docker push harbor.kunxiangtech.com:8443/kb-platform/workflow:${VERSION}
 docker push harbor.kunxiangtech.com:8443/kb-platform/gateway:${VERSION}
+docker push harbor.kunxiangtech.com:8443/kb-platform/frontend:${VERSION}
